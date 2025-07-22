1938fcf1a37fa010f2b885a558ca22c83a22eefa
glimaalv <gusta.pessoal02@gmail.com>
2025-07-22 16:44:10 -0300

    começando adicionar funcionalidades para segunda conta

--- src/Main.java
+++ src/Main.java
@@ -3,59 +3,92 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
         int operation = 0;
-        double cash = 0;
-        String userData = """
-                ------------------------------
-                Dados do cliente:
-                
-                Nome:          Gustavo Alves
-                Tipo de Conta: Conta Corrente
-                Saldo Inicial: %f
-                ------------------------------
-                
-                """.formatted(cash);
+        double checkingCash = 0;
+        double investedCash = 0;
+        double yearInvestedCash = investedCash + (investedCash*0.12);
 
-        String textOptions = """
+        String textAccountType = """
+                Tipos de Conta:
                 
-                Operações:
-                
-                1- Consultar Saldo
-                2- Receber Valor
-                3- Transferir Valor
-                4- Sair
+                1- Conta Corrente
+                2- Conta Poupança
+                3- Fechar App
                 
                 Digite a opção desejada:
                 """;
 
-        System.out.println(userData);
+        String textOperations = """
+                
+                Operações:
+                
+                1- Página Inicial
+                2- Consultar Saldo
+                3- Receber Valor
+                4- Transferir Valor Para A Outra Conta
+                5- Sair Desta Conta
+                
+                Digite a opção desejada:
+                """;
 
-        while (operation != 4){
-            System.out.println(textOptions);
+        System.out.println("Para inicizar, digite seu nome:");
+        String name = scanner.nextLine();
+
+        String userDataCheckingAccount = """
+                ------------------------------
+                Dados do cliente:
+                
+                Nome:          %s
+                Tipo de Conta: Conta Corrente
+                Saldo Atual: %.2f
+                ------------------------------
+                
+                """.formatted(name, checkingCash);
+
+        String userDataInvestingAccount = """
+                ------------------------------
+                Dados do cliente:
+                
+                Nome:           %s
+                Tipo de Conta:  Conta Poupança
+                Saldo Atual:    %.2f
+                Saldo Em 1 Ano: %.2f
+                """.formatted(name, investedCash, yearInvestedCash);
+
+        System.out.println(textAccountType);
+        while (operation != 3){
+
+        }
+
+        System.out.println(userDataCheckingAccount);
+
+        while (operation != 5){
+            System.out.println(textOperations);
             operation = scanner.nextInt();
 
             switch (operation) {
                 case 1:
-                    System.out.println("Seu saldo atual é de: " + cash);
-                    break;
+                    System.out.println(userDataCheckingAccount);
                 case 2:
-                    System.out.println("Digite o valor a ser recebido:");
-                    double addedValue = scanner.nextDouble();
-                    cash = cash + addedValue;
-                    System.out.println("Seu saldo atual é de: " + cash);
+                    System.out.println("Seu saldo atual é de: " + checkingCash);
                     break;
                 case 3:
-                    System.out.println("Digite o valor a ser transferido:");
-                    double subtractedValue = scanner.nextDouble();
-                    if (subtractedValue > cash){
-                        System.out.println("Você não possui saldo suficiente para realizar esta transferência!");
-                    } else{
-                        cash = cash - subtractedValue;
-                        System.out.println("Dinheiro transferido, seu saldo agora é de: " + cash);
-                    }
+                    System.out.println("Digite o valor a ser recebido:");
+                    double addedValue = scanner.nextDouble();
+                    checkingCash = checkingCash + addedValue;
+                    System.out.println("Seu saldo atual é de: " + checkingCash);
                     break;
                 case 4:
+                    System.out.println("Digite o valor a ser transferido:");
+                    double subtractedValue = scanner.nextDouble();
+                    if (subtractedValue > checkingCash){
+                        System.out.println("Você não possui saldo suficiente para realizar esta transferência!");
+                    } else{
+                        checkingCash = checkingCash - subtractedValue;
+                        System.out.println("Dinheiro transferido, seu saldo agora é de: " + checkingCash);
+                    }
+                    break;
+                case 5:
                     break;
                 default:
                     System.out.println("Opção incorreta, digite novamente:");
