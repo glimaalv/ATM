a9b297b0b98244368e8a4adfd93e590d95f8cdf1
glimaalv <gusta.pessoal02@gmail.com>
2025-07-23 14:54:49 -0300

    dividindo as funcionalidades em corrente e poupança

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int operation = 0;
-        double checkingCash = 0;
         double investedCash = 0;
-        double yearInvestedCash = investedCash + (investedCash*0.12);
+        double yearInvestedCash = investedCash + (investedCash * 0.12);
         int inicialOption = 0;
+        double transferValue = 0;
 
         String textInicialOptions = """
                 Tipos de Conta:
@@ -19,14 +19,14 @@ public class Main {
                 Digite a opção desejada:
                 """;
 
-        String textOperations = """
+        String textAccountOptions = """
                 
                 Operações:
                 
-                1- Página Inicial
-                2- Consultar Saldo
-                3- Receber Valor
-                4- Transferir Valor Para A Outra Conta
+                1- Consultar Saldo
+                2- Receber Valor
+                3- Transferir Entre Contas
+                4- Transferir Para Fora
                 5- Sair Desta Conta
                 
                 Digite a opção desejada:
@@ -34,6 +34,8 @@ public class Main {
 
         System.out.println("Para inicizar, digite seu nome:");
         String name = scanner.nextLine();
+        System.out.println("Primeiro depósito de sua Conta Corrente:");
+        double checkingCash = scanner.nextDouble();
 
         String textDataCheckingAccount = """
                 ------------------------------
@@ -56,36 +58,44 @@ public class Main {
                 Saldo Em 1 Ano: %.2f
                 """.formatted(name, investedCash, yearInvestedCash);
 
-        do{
+        do {
             System.out.println(textInicialOptions);
             inicialOption = scanner.nextInt();
 
             if (inicialOption == 1) {
                 System.out.println(textDataCheckingAccount);
                 while (operation != 5) {
-                    System.out.println(textOperations);
+                    System.out.println(textAccountOptions);
                     operation = scanner.nextInt();
 
                     switch (operation) {
                         case 1:
-                            System.out.println(textInicialOptions);
+                            System.out.println("Seu saldo atual na conta corrente é de: " + checkingCash);
                             break;
                         case 2:
+                            System.out.println("Digite o valor a ser recebido:");
+                            transferValue = scanner.nextDouble();
+                            checkingCash = checkingCash + transferValue;
                             System.out.println("Seu saldo atual é de: " + checkingCash);
                             break;
                         case 3:
-                            System.out.println("Digite o valor a ser recebido:");
-                            double addedValue = scanner.nextDouble();
-                            checkingCash = checkingCash + addedValue;
-                            System.out.println("Seu saldo atual é de: " + checkingCash);
+                            System.out.println("Digite o valor a ser transferido para a Conta Poupança:");
+                            transferValue = scanner.nextDouble();
+                            if (transferValue > checkingCash) {
+                                System.out.println("Você não possui saldo suficiente para realizar esta transferência!");
+                            } else {
+                                checkingCash = checkingCash - transferValue;
+                                investedCash = investedCash + transferValue;
+                                System.out.println("Dinheiro transferido, seu saldo agora é de: " + checkingCash);
+                            }
                             break;
                         case 4:
                             System.out.println("Digite o valor a ser transferido:");
-                            double subtractedValue = scanner.nextDouble();
-                            if (subtractedValue > checkingCash) {
+                            transferValue = scanner.nextDouble();
+                            if (transferValue > checkingCash) {
                                 System.out.println("Você não possui saldo suficiente para realizar esta transferência!");
                             } else {
-                                checkingCash = checkingCash - subtractedValue;
+                                checkingCash = checkingCash - transferValue;
                                 System.out.println("Dinheiro transferido, seu saldo agora é de: " + checkingCash);
                             }
                             break;
@@ -95,23 +105,35 @@ public class Main {
                             System.out.println("Opção incorreta, digite novamente:");
                     }
                 }
-
+            } else if (inicialOption == 2) {
                 System.out.println(textDataInvestingAccount);
                 while (operation != 5) {
-                    System.out.println(textOperations);
+                    System.out.println(textAccountOptions);
                     operation = scanner.nextInt();
 
                     switch (operation) {
                         case 1:
-                            System.out.println(textInicialOptions);
+                            System.out.println("Seu saldo atual na Poupança é de: " + investedCash);
                             break;
                         case 2:
-                            System.out.println("Seu saldo atual é de: " + checkingCash);
+                            System.out.println("Digite o valor a ser recebido:");
+                            transferValue = scanner.nextDouble();
+                            if (transferValue > checkingCash){
+                                System.out.println("Você não possui saldo para essa transferência!");
+                            }else{
+                                investedCash = investedCash + transferValue;
+                                System.out.println("Dinheiro recebido, seu saldo Poupança atual é de: " + investedCash);
+                            }
                             break;
                         case 3:
-                            System.out.println("Digite o valor a ser recebido:");
-                            double addedValue = scanner.nextDouble();
-                            checkingCash = checkingCash + addedValue;
+                            System.out.println("Digite o valor a ser transferido para a Conta Corrente:");
+                            transferValue = scanner.nextDouble();
+                            if (transferValue > investedCash){
+                                System.out.println("Você não possui saldo para essa transferência!");
+                            }else{
+                                investedCash = investedCash - transferValue;
+                            }
+                            checkingCash = checkingCash + transferValue;
                             System.out.println("Seu saldo atual é de: " + checkingCash);
                             break;
                         case 4:
@@ -130,44 +152,9 @@ public class Main {
                             System.out.println("Opção incorreta, digite novamente:");
                     }
                 }
-            }else if (inicialOption == 2) {
-                System.out.println(textDataInvestingAccount);
-                while (operation != 5) {
-                    System.out.println(textOperations);
-                    operation = scanner.nextInt();
-
-                    switch (operation) {
-                        case 1:
-                            System.out.println(textInicialOptions);
-                            break;
-                        case 2:
-                            System.out.println("Seu saldo atual é de: " + checkingCash);
-                            break;
-                        case 3:
-                            System.out.println("Digite o valor a ser recebido:");
-                            double addedValue = scanner.nextDouble();
-                            checkingCash = checkingCash + addedValue;
-                            System.out.println("Seu saldo atual é de: " + checkingCash);
-                            break;
-                        case 4:
-                            System.out.println("Digite o valor a ser transferido:");
-                            double subtractedValue = scanner.nextDouble();
-                            if (subtractedValue > checkingCash) {
-                                System.out.println("Você não possui saldo suficiente para realizar esta transferência!");
-                            } else {
-                                checkingCash = checkingCash - subtractedValue;
-                                System.out.println("Dinheiro transferido, seu saldo agora é de: " + checkingCash);
-                            }
-                            break;
-                        case 5:
-                            break;
-                        default:
-                            System.out.println("Opção incorreta, digite novamente:");
-                    }
-                }
-            } else{
+            } else {
                 break;
             }
-        }while (inicialOption != 3);
+        } while (inicialOption != 3);
     }
 }
